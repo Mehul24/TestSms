93e8ce766119cc11be0c95ee6c51e3545fc65bbe
Mehul Mittal <mehul.usc@gmail.com>
2014-04-01 02:02:02 -0700

    a

--- www/ContactUs.js
+++ www/ContactUs.js
@@ -1,41 +1,28 @@
 var contact = function(){};
 
-contact.textUs = function(){
-    console.log(type);
+contact.textUs = function(successCallback, errorCallback) {
     console.log('inside text contact');
-    var text = {
-        createEvent: function(successCallback, errorCallback) {
-            cordova.exec(
-                successCallback, // success callback function
-                errorCallback, // error callback function
-                'Contact', 
-                'openSmsView', // with this action name
-                [{   
-                }]
-            );
-        }
-    };     
+    cordova.exec(
+        successCallback, // success callback function
+        errorCallback, // error callback function
+        'Contact', 
+        'openSmsView', // with this action name
+        [{   
+        }]
+    );    
 };
 
-contact.callUs = function() {
+contact.callUs = function(successCallback, errorCallback) {
     console.log('inside call contact');
-    var call = {
-        createEvent: function(successCallback, errorCallback) {
-            cordova.exec(
-                successCallback, // success callback function
-                errorCallback, // error callback function
-                'Contact', 
-                'openCallView', // with this action name
-                [{   
-                }]
-            ); 
-        }
-    };  
+    cordova.exec(
+        successCallback, // success callback function
+        errorCallback, // error callback function
+        'Contact', 
+        'openCallView', // with this action name
+        [{   
+        }]
+    ); 
 };
     
-    
-
-
-
 module.exports = contact;
 // module.exports = call;
